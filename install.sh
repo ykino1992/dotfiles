0c1b327f44f0a35210acaf1d9416d6298a010b75
Yusuke Kinoshita <underfox.underdog@gmail.com>
2016-06-15 09:23:32 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.ctags ~/.ctags
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
