e60343fe14daa26cdfb7a1f571201b3c727cb455
Yusuke Kinoshita <underfox.underdog@gmail.com>
2016-06-15 14:09:49 +0900

    Modify gitconfig

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ ln -sf ~/dotfiles/.ctags ~/.ctags
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore
+git config --global core.editor vim
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
