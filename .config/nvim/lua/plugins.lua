a51bc3ffb25673a471e94d077ed98cc2746c3c17
Yusuke Kinoshita <yusuke.kinoshita@knowledgelabo.com>
2024-11-25 22:14:05 +0900

    feat(plugins.lua): Lspsagaのキー割り当てを更新しAvanteプラグインを追加 新しいAI連携プラグインAvanteを追加し、コードアクションのキーを変更。

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -427,7 +427,7 @@ return {
       set("n", "g[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- 前のエラーに移動
       set("n", "g]", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- 次のエラーに移動
       set("n", "rn", "<cmd>Lspsaga rename<CR>", opts)               -- 参照先までリネーム
-      set("n", "ma", "<cmd>Lspsaga code_action<CR>", opts)          -- コードアクション
+      set("n", "ga", "<cmd>Lspsaga code_action<CR>", opts)          -- コードアクション
     end,
     dependencies = {
       'nvim-treesitter/nvim-treesitter',
@@ -442,5 +442,58 @@ return {
     config = function()
       vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { noremap = true, silent = true })
     end
+  },
+
+  -- Avante
+  -- AIと連携してコードを書ける
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "openai",
+      behaviour = {
+        auto_suggestions = false
+      }
+      -- add any opts here
+    },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   }
 }
